0c5749ce49eba313e1330c71e61144175e96e7f0
Aryan <aryan0059.becse24@chitkara.edu.in>
2025-11-24 10:09:39 +0530

    model

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import { Ziva } from "./components/Ziva"; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // h-screen: 100vh, w-full: 100% width
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 5], fov: 30 }}
+      className="h-screen w-full bg-[#ececec]" 
+    >
+      <Suspense fallback={null}>
+        <Environment preset="mjolnir" />
+        
+        {/* Position the model slightly lower to center the face */}
+        <group position={[0, -1, 0]}>
+            <Ziva />
+        </group>
+
+      </Suspense>
+
+      <OrbitControls />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
